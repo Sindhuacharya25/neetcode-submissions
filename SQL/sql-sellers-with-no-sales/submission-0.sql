5fd0d28ebc8a245653260ad6afafdae5e090413e
Sindhuacharya25 <sindhuacharya25@gmail.com>
2026-06-26 21:23:45 +0530

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+SELECT s.seller_name
+FROM seller AS s
+LEFT JOIN orders AS o
+  ON s.seller_id = o.seller_id
+  AND o.sale_date BETWEEN '2020-01-01' AND '2020-12-31'
+WHERE o.seller_id IS NULL
+ORDER BY seller_name ASC;
+
